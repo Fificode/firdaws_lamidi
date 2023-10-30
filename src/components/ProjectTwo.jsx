8737fc4b868776ae8ce7bf643178976167309d91
Fificode <lamidifirdaws@gmail.com>
2023-10-30 22:57:21 +0100

    added target blank to project website links

--- src/components/ProjectTwo.jsx
+++ src/components/ProjectTwo.jsx
@@ -21,7 +21,7 @@ const ProjectTwo = ({header, stackone, stackthree, stackfour, stackfive, explana
        <div className="flex flex-row justify-center my-[15px] ">
 <div className="mx-[10px]">
   <div className="border-[1px] px-[20px] py-[5px] md:py-[10px] rounded-[5px] border-light-black flex justify-between items-center">
-    <a href="https://react-tailwind-teta.netlify.app/" className="text-center text-[14px] md:text-[17px]">Visit website</a>
+    <a href="https://react-tailwind-teta.netlify.app/" target='_blank' rel="noreferrer" className="text-center text-[14px] md:text-[17px]">Visit website</a>
      <div className="mx-[3px]"> <svg className="w-[20px] h-[15px] md:w-[25px] md:h-[25px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
   <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
 </svg>
